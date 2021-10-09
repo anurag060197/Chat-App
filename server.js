e9114d4958b9dc50f69ff7590b85346c1fa31618
Anurag Sahu <anuragsahu060197@gmail.com>
2021-10-09 11:26:14 +0530

    color change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const server = http.createServer(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3020;
 
 app.use(express.static(__dirname+'/public'))
 
